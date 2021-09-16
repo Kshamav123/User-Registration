a92122afbb64588d3524f00cd5abf3a547520c95
Kshamav123 <kshamavidya1999@gmail.com>
2021-09-16 19:28:35 +0530

    Added UC3

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,6 +7,7 @@ public class UserRegistration {
 	public static Scanner sc = new Scanner(System.in);
 	public final String firstName = "[A-Z][a-z]{2,}$";
 	public final String lastName = "[A-Z][a-z]{2,}$";
+	public final String email = "[a-z0-9_]*[.a-z0-9_]*?@[a-z]*.(co|com)(.[a-z])?$";
 	
 	public static void main (String[] args) {
 		UserRegistration user= new UserRegistration();
@@ -14,6 +15,9 @@ public class UserRegistration {
 		String userFirstName = sc.next();
 		System.out.println("Enter your Last Name: ");
 		String userLastName = sc.next();
+		System.out.println("Enter your email id: ");
+		String userEmail = sc.next();
+		
 		if(user.firstName(userFirstName))
 		{
 			System.out.println("The First Name entered is Valid.");
@@ -28,7 +32,13 @@ public class UserRegistration {
 		else {
 			System.out.println("Last name is Invalid.");
 		}
-		
+		if(user.lastName(userLastName))
+		{
+			System.out.println("The Email entered is Valid.");
+		}
+		else {
+			System.out.println("Email is Invalid.");
+		}
 	}
 	
 	public boolean firstName(String userFirstName) {
@@ -39,5 +49,9 @@ public class UserRegistration {
 	public boolean lastName(String userLastName) {
 		Pattern pattern = Pattern.compile(lastName);
 		return pattern.matches(lastName, userLastName);
+	}
+	public boolean email(String userEmail) {
+		Pattern pattern = Pattern.compile(email);
+		return pattern.matches(email, userEmail);
 	}
 }
