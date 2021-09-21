ce82dc495a07bfac121b3eaaba27ac19b78bf4f4
Kshamav123 <kshamavidya1999@gmail.com>
2021-09-21 17:35:43 +0530

    Commented the code

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -8,74 +8,108 @@ import org.junit.Test;
 import com.userregistration.UserRegistration;
 
 public class UserRegistrationTest {
- @Test
- public void firstNameWhenInputIsValid() 
-	{
+	/**
+	 * Test case when its valid
+	 */
+	@Test
+	public void firstNameWhenInputIsValid() {
 		UserRegistration obj = new UserRegistration();
 		boolean result = obj.firstNameVal("Bill");
-		assertEquals(true,result);		
+		assertEquals(true, result);
 	}
- @Test
- public void firstNameWhenSmallNoIsInvalid() 
-	{
+
+	/**
+	 * FirstName should have Starting capital and minimum of 3 characters
+	 */
+	@Test
+	public void firstNameWhenSmallNoIsInvalid() {
 		UserRegistration obj = new UserRegistration();
-		boolean result = obj.firstNameVal("kshama");
-		assertEquals(true,result);	
+		boolean result = obj.firstNameVal("kshama3");
+		assertEquals(false, result);
 	}
- @Test
- public void lastNameWhenInputIsValid() 
-	{
+
+	/**
+	 * Test case when its valid
+	 */
+	@Test
+	public void lastNameWhenInputIsValid() {
 		UserRegistration obj = new UserRegistration();
 		boolean result = obj.lastNameVal("Williams");
-		assertEquals(true,result);	
+		assertEquals(true, result);
 	}
- @Test
- public void LastNameWhenSmallNoIsInvalid() 
-	{
+
+	/**
+	 * LastName should have Starting capital and minimum of 3 characters
+	 */
+
+	@Test
+	public void LastNameWhenSmallNoIsInvalid() {
 		UserRegistration obj = new UserRegistration();
 		boolean result = obj.lastNameVal("williams2");
-		assertEquals(true,result);	
+		assertEquals(false, result);
 	}
- @Test
- public void EmailWhenInputIsValid() {
+
+	/**
+	 * Test case when its valid
+	 */
+	@Test
+	public void EmailWhenInputIsValid() {
 		UserRegistration obj = new UserRegistration();
 		boolean result = obj.emailVal("gvxg@88gmil.com");
-		assertEquals(true,result);
-}
- @Test
- public void EmailWhenInputIsInvalid() 
-	{
+		assertEquals(true, result);
+	}
+
+	/**
+	 * Email has 3 manadatory parts with abc,bl and co with precise @ and .
+	 * positions (abc.xyz@bl.co.in) where rest are optional
+	 */
+	@Test
+	public void EmailWhenInputIsInvalid() {
 		UserRegistration obj = new UserRegistration();
 		boolean result = obj.emailVal("-ab21.yahoo.co.in");
-		assertEquals(true,result);	
+		assertEquals(false, result);
 	}
- @Test
- public void MobileNoWhenInputIsValid() 
-	{
+
+	/**
+	 * Test case when its valid
+	 */
+
+	@Test
+	public void MobileNoWhenInputIsValid() {
 		UserRegistration obj = new UserRegistration();
 		boolean result = obj.phNoVal("91 1234567893");
-		assertEquals(true,result);	
+		assertEquals(true, result);
 	}
- @Test
- public void MobileNoWhenLettersNoSpaceIsInvalid() 
-	{
+
+	/**
+	 * Mobile number should have country code followed by space and 10 digits
+	 */
+	@Test
+	public void MobileNoWhenLettersNoSpaceIsInvalid() {
 		UserRegistration obj = new UserRegistration();
 		boolean result = obj.phNoVal("123456782d3");
-		assertEquals(true,result);	
+		assertEquals(false, result);
 	}
-@Test
- public void PasswordWhenInputIsValid() 
-	{
+
+	/**
+	 * Test case is valid
+	 */
+	@Test
+	public void PasswordWhenInputIsValid() {
 		UserRegistration obj = new UserRegistration();
 		boolean result = obj.passwordVal("fh5@Sjkn645gv");
-		Assert.assertEquals( true,result);
+		Assert.assertEquals(true, result);
 	}
-@Test
-public void PasswordWhenNospecialCharacter_NoCapsIsInvalid() 
-{
-	UserRegistration obj = new UserRegistration();
-	boolean result = obj.passwordVal("jhsdbjak");
-	assertEquals(true,result);	
-}
+
+	/**
+	 * Password should have minimum 8 characters atleast 1 number and upper case,
+	 * exactly 1 Special character
+	 */
+	@Test
+	public void PasswordWhenNospecialCharacter_NoCapsIsInvalid() {
+		UserRegistration obj = new UserRegistration();
+		boolean result = obj.passwordVal("jhsdbjak");
+		assertEquals(false, result);
+	}
 
 }
